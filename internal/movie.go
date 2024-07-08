559902a10b47f9348a7c43804049b5396468104a
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2024-07-07 22:13:22 -0300

    ch 3.3: hidding fields from JSON object

--- internal/movie.go
+++ internal/movie.go
@@ -5,10 +5,10 @@ import "time"
 // Annotate the Movie struct with struct tags to control how the keys appear in the JSON-encoded output.
 type Movie struct {
 	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"-"` // Use the - directive
 	Title     string    `json:"title"`
-	Year      int32     `json:"year"`
-	Runtime   int32     `json:"runtime"`
-	Genres    []string  `json:"genres"`
+	Year      int32     `json:"year,omitempty"`    // Add the omitempty directive
+	Runtime   int32     `json:"runtime,omitempty"` // Add the omitempty directive
+	Genres    []string  `json:"genres,omitempty"`  // Add the omitempty directive
 	Version   int32     `json:"version"`
 }
