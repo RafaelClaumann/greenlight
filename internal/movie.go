ac90e5ac261aa91bf23d5fbbaaab19032d0eead6
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2024-07-08 21:23:39 -0300

    ch 3.5: customizing runtime JSON field

--- internal/movie.go
+++ internal/movie.go
@@ -1,14 +1,18 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"greenlight.claumann.net/internal/data"
+)
 
 // Annotate the Movie struct with struct tags to control how the keys appear in the JSON-encoded output.
 type Movie struct {
-	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"-"` // Use the - directive
-	Title     string    `json:"title"`
-	Year      int32     `json:"year,omitempty"`    // Add the omitempty directive
-	Runtime   int32     `json:"runtime,omitempty"` // Add the omitempty directive
-	Genres    []string  `json:"genres,omitempty"`  // Add the omitempty directive
-	Version   int32     `json:"version"`
+	ID        int64        `json:"id"`
+	CreatedAt time.Time    `json:"-"` // Use the - directive
+	Title     string       `json:"title"`
+	Year      int32        `json:"year,omitempty"` // Add the omitempty directive
+	Runtime   data.Runtime `json:"runtime,omitempty"`
+	Genres    []string     `json:"genres,omitempty"` // Add the omitempty directive
+	Version   int32        `json:"version"`
 }
